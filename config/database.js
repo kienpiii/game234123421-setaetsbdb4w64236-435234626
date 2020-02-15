c1bd9bdece1e0da44ad9a4523cb69f28bfeaeb76
kienpiii <61084096+kienpiii@users.noreply.github.com>
2020-02-15 20:11:16 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+	'url': 'mongodb+srv://cluster0-z9jgm.mongodb.net/test',
+	'options': {
+		'user':   'kgame',
+		'pass':   'kyhbn94',
+		'dbName': 'game',
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
---
+++ config/sms.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+	'URL':       'https://api.wavecell.com/sms/v1/vinclub_7hHF0_hq/single',
+	'Author':    'XMJ6kusQCQdbY6nigaOkqa6KhUbYkEZxokg28xITY',
+	'Brandname': 'Vin99Club',
+}
